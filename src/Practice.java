c146660952b8bb965fd8baff6c987ac73128c4d0
Rajeshvig42 <125672492+Rajeshvig42@users.noreply.github.com>
2023-02-17 08:38:56 -0600

    Mybasiccode

---
+++ src/Practice.java
@@ -0,0 +1,45 @@
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	class Addition
+	{
+		public void add()
+		{
+			int a,b,c;
+			a=10;
+			b=20;
+			c=a+b;
+			System.out.println(c);
+		}
+	}
+	class Subtratction
+	{
+		public void sub()
+		{
+			int a,b,c;
+			a=20;
+			b=10;
+			c=a-b;
+			System.out.println(c);
+		}
+	}
+
+	public class MathOperation {
+		
+
+		public static void main(String[] args) {
+			// if anything you want to print so call it inside main block 
+	 System.out.println("Hi From Main Class");
+	  		Addition addition=new Addition();
+	  		addition.add();
+	  		Subtratction subtratction=new Subtratction();
+	  		subtratction.sub();
+	 
+		}
+
+	}
+}
---
+++ src/classwork.java
@@ -0,0 +1,10 @@
+
+public class classwork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+     system.out.prntln(" Hi from first class");
